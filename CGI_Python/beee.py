2aa04fe88e83d0ca09294a1eaf389474b4ec2771
chrisluo5311 <chrislo5311@gmail.com>
2023-12-27 10:15:54 +0800

    update 1227

--- CGI_Python/beee.py
+++ CGI_Python/beee.py
@@ -1,7 +1,18 @@
-from pyfirmata import Arduino
+import serial
+import time
 
-port ='COM8'
-board = Arduino(port)
+def setup():
+    global s
+    s = serial.Serial("COM8",9600)
 
-pin = 13
-board.digital[pin].write(1)
+def loop():
+    s.write(1)
+    time.sleep(1)
+
+    s.write("0")
+    time.sleep(1)
+
+if __name__ == '__main__':
+    setup()
+    while True:
+        loop()
---
+++ CGI_Python/data_init.sql
@@ -0,0 +1,8 @@
+create database embedded_system
+create table embedded_system.user
+(
+    user_id integer        null,
+    name    varchar(21845) null,
+    passwd  integer        null,
+    img_url varchar(21845) null
+);
